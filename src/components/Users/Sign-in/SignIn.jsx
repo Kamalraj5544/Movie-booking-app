b391a8b73b890343ec714e94a7cc81eca64c3a09
Kamal raj k <kamalraj5544@gmail.com>
2023-03-02 17:10:19 +0530

    added signIn page

--- src/components/Users/Sign-in/SignIn.jsx
+++ src/components/Users/Sign-in/SignIn.jsx
@@ -1,9 +1,69 @@
-
+import "./SignIn.scss";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="wrapper">
+      <div className="conatiner">
+        <div className="account-area">
+          <div className="account-header text-center">
+            <div className="teal">HELLO</div>
+            <h2 className="title">
+              <span>W</span>ELCOME BAC<span>K</span>
+            </h2>
+          </div>
+          <div className="account-form">
+            <form>
+              <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label">
+                  Email <span className="mandatory">*</span>
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter Your Email"
+                />
+              </div>
+              <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">
+                  Password<span className="mandatory">*</span>
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="mb-3 form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                  checked
+                />
+                <div className="space-bet">
+                  <label className="form-check-label" for="exampleCheck1">
+                    Remember Password
+                  </label>
+                  <a className="fg-Pass">Forgot Password</a>
+                </div>
+              </div>
+              <div className="text-center">
+                <button type="submit" className="btn join-btn">
+                  SIGN IN
+                </button>
+                <div className="login-section">
+                  Don't have a account? <a className="terms">Sign up now</a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
