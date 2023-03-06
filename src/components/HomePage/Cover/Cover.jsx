558cba8e84260d0886553eb64d510d8296f90b7b
Kamal raj k <kamalraj5544@gmail.com>
2023-03-07 00:16:15 +0530

    added searchPanel

--- src/components/HomePage/Cover/Cover.jsx
+++ src/components/HomePage/Cover/Cover.jsx
@@ -5,11 +5,15 @@ const Cover = () => {
     <div className="coverContainer">
       <div className="bg-img"></div>
       <div className="textcontainer">
-        <h1 className="cover-header">
-          GET <span className="teal-color">MOVIE</span> TICKETS
-        </h1>
-        <p>Buy movie tickets in advance, find movie times</p>
-        <p>watch trailers, read movie reviews and much more</p>
+        <div>
+          <h1 className="cover-header">
+            GET <span className="teal-color">MOVIE</span> TICKETS
+          </h1>
+        </div>
+        <div>
+          <p>Buy movie tickets in advance, find movie times</p>
+          <p>watch trailers, read movie reviews and much more</p>
+        </div>
       </div>
     </div>
   );
--- src/components/HomePage/SearchPanel/SearchPanel.jsx
+++ src/components/HomePage/SearchPanel/SearchPanel.jsx
@@ -22,51 +22,50 @@ const SearchPanel = () => {
   return (
     <div className="search-ticket-section">
       <div className="container">
-        <div className="search-section-bgImg">
-          <div className="search-header text-center">
-            <p className="welcome-section">
-              <span className="teal-color">WELCOME TO BOOK MY TICKET </span>
-              {/* <span className="red-color">B</span>OOK{" "}
-              <span className="red-color">MY</span> TICKE
-              <span className="red-color">T</span> */}
-            </p>
-            <p>WHAT ARE YOU LOOKING FOR</p>
-          </div>
-          <div className="search-box container">
-            <form className="form-container">
-              <div className="form-group">
-                <div className="search-input">
-                  <input
-                    type="text"
-                    placeholder="Search for Movies"
-                    className="input-box"
-                  />
-                  <span>
-                    <BsSearch />
-                  </span>
-                </div>
-              </div>
-              <div className="form-group">
-                <Dropdown
-                  value={selectedCity}
-                  onChange={(e) => setSelectedCity(e.value)}
-                  options={cities}
-                  optionLabel="name"
-                  placeholder="Select a City"
-                  className="w-full md:w-14rem "
+        <div className="search-section-bgImg"></div>
+        <div className="search-header text-center">
+          <p className="welcome-section">
+            <span className="teal-color">WELCOME TO BOOK MY TICKET </span>
+            {/* <span className="red-color">B</span>OOK{" "}
+            <span className="red-color">MY</span> TICKE
+            <span className="red-color">T</span> */}
+          </p>
+          <p>WHAT ARE YOU LOOKING FOR</p>
+        </div>
+        <div className="search-box container">
+          <form className="form-container">
+            <div className="form-group">
+              <div className="search-input">
+                <input
+                  type="text"
+                  placeholder="Search for Movies"
+                  className="input-box"
                 />
+                <span>
+                  <BsSearch />
+                </span>
               </div>
-              <div className="form-group ">
-                <Calendar
-                  value={date}
-                  placeholder="Select a Date"
-                  onChange={(e) => setDate(e.value)}
-                  showIcon
-                  className="w-full md:w-14rem"
-                />
-              </div>
-            </form>
-          </div>
+            </div>
+            <div className="form-group">
+              <Dropdown
+                value={selectedCity}
+                onChange={(e) => setSelectedCity(e.value)}
+                options={cities}
+                optionLabel="name"
+                placeholder="Select a City"
+                className="w-full md:w-14rem "
+              />
+            </div>
+            <div className="form-group ">
+              <Calendar
+                value={date}
+                placeholder="Select a Date"
+                onChange={(e) => setDate(e.value)}
+                showIcon
+                className="w-full md:w-14rem"
+              />
+            </div>
+          </form>
         </div>
       </div>
     </div>
