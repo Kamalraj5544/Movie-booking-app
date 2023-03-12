911810f3de9adfb2c47511d80c327a74aac0315e
Kamal raj k <kamalraj5544@gmail.com>
2023-03-12 21:14:34 +0530

    added moviedetails page

---
+++ src/components/MovieDetails/MovieDetailsHeader/MovieDeatilsHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieDeatilsHeader = () => {
+  return (
+    <div>MovieDeatilsHeader</div>
+  )
+}
+
+export default MovieDeatilsHeader
--- src/components/Routing/RoutingPages.jsx
+++ src/components/Routing/RoutingPages.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
-import Home from '../HomePage/Home/Home';
-import SignIn from '../Users/Sign-in/SignIn';
-import SignUp from '../Users/Sign-up/SignUp';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../HomePage/Home/Home";
+import MovieDetailsHome from "../MovieDetails/MovieDetailsHome/MovieDetailsHome";
+import SignIn from "../Users/Sign-in/SignIn";
+import SignUp from "../Users/Sign-up/SignUp";
 
 const RouteingPages = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-      <Route path="/signin" element={ <SignIn />} />
-      <Route  path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/movie" element={<MovieDetailsHome />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default RouteingPages
+export default RouteingPages;
