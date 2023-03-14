c0189d34f5fb638342eaeb379af7e64cf50288ae
Kamal raj k <kamalraj5544@gmail.com>
2023-03-14 22:42:19 +0530

    updated user review with api

--- src/components/MovieDetails/UserReview/UserReview.jsx
+++ src/components/MovieDetails/UserReview/UserReview.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 import { TabView, TabPanel } from "primereact/tabview";
-import { Avatar } from 'primereact/avatar';
+import { Avatar } from "primereact/avatar";
 
 import "./UserReview.scss";
 
@@ -22,7 +22,7 @@ const UserReview = () => {
   }, []);
 
   return (
-    <div className="container ">
+    <div className="container">
       <TabView>
         <TabPanel header="Summary">
           <h2>Description</h2>
@@ -34,17 +34,42 @@ const UserReview = () => {
           </div>
         </TabPanel>
         <TabPanel header="User review(87)">
-        <div className="user-review-container">
-        <div className="user-img">
-        <Avatar image="https://e7.pngegg.com/pngimages/136/22/png-clipart-user-profile-computer-icons-girl-customer-avatar-angle-heroes-thumbnail.png" size="xlarge" shape="circle" />
-        </div>
-        <div className="user-details">
-            <h2>Kamal</h2>
-            <div>13 days ago</div>
-            <div><span className="pi pi-check-circle"></span> Verfied review</div>
-        </div>
-        </div>
-        
+          {movie.userReviews &&
+            movie.userReviews.map((review, i) => (
+              <div
+                className="user-review-container flex flex-wrap"
+                key={i + review}
+              >
+                <div className="user-details-container">
+                  <div className="user-img">
+                    <Avatar
+                      image={review.userProfilePicUrl}
+                      size="xlarge"
+                      shape="circle"
+                    />
+                  </div>
+                  <div className="user-details">
+                    <h2>{review.name}</h2>
+                    <div>{review.date}</div>
+                    <div>
+                      <span className="pi pi-check-circle"></span> Verfied
+                      review
+                    </div>
+                  </div>
+                </div>
+                <div className="movie-review-container">
+                  <div className="rate-it">
+                    <i className="pi pi-heart-fill"></i>
+                    <i className="pi pi-heart-fill"></i>
+                    <i className="pi pi-heart-fill"></i>
+                    <i className="pi pi-heart-fill"></i>
+                    <i className="pi pi-heart-fill"></i>
+                  </div>
+                  <h4>Awesome Movie</h4>
+                  <div>{review.comment}</div>
+                </div>
+              </div>
+            ))}
         </TabPanel>
       </TabView>
     </div>
