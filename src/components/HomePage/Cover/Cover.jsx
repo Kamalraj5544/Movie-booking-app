adcbab909169e3eec870744d5384d5b751be9b82
Kamal raj k <kamalraj5544@gmail.com>
2023-04-08 00:04:51 +0530

    added typewriter effect

--- src/components/HomePage/Cover/Cover.jsx
+++ src/components/HomePage/Cover/Cover.jsx
@@ -1,5 +1,7 @@
 import "./Cover.scss";
 
+import Typewriter from "typewriter-effect";
+
 const Cover = () => {
   return (
     <div className="coverContainer">
@@ -8,6 +10,24 @@ const Cover = () => {
           <h1 className="cover-header">
             GET <span className="teal-color">MOVIE</span> TICKETS
           </h1>
+          <h3 className="text-center teal-color pb-4">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("SAFE")
+                  .pause(3000)
+                  .deleteAll()
+                  .typeString("SECURE")
+                  .pause(3000)
+                  .deleteAll()
+                  .typeString("RELAIBLE TICKETING")
+                  .pause(3000)
+                  .deleteAll()
+                  .typeString("Safe,Secure,Reliable Ticketing")
+                  .start();
+              }}
+            />
+          </h3>
         </div>
         <div>
           <p>Buy movie tickets in advance, find movie times</p>
